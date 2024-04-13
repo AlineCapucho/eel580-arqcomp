39452580d0bcdda02e7ddfe353bad1e051e2a9a5
aline.capucho <aline.capucho@olxbr.com>
2024-04-13 18:24:25 -0300

    feat: rename file and fix errors

--- dgemm/chapter_3_v1.c
+++ dgemm/chapter_3_v1.c
@@ -8,13 +8,10 @@ void dgemm (size_t n, double* A, double* B, double* C) {
   for (size_t i = 0; i < n; ++i) {
     for (size_t j = 0; j < n; ++j) {
       double cij = C[i + j*n]; //cij = C[i][j]
-      // o correto não seria cij = C[i*n + j]?
       for (size_t k = 0; k < n; ++k) {
         cij += A[i + k*n] * B[k + j*n]; // *cij += A[i][k] * B[k][j]
-        // o correto não seria cij += A[i * n + k] * B[k * n + j]?
       }
       C[i+j*n] = cij; // C[i][j] = cij
-      // o correto não seria C[i * n + j] = cij;?
     }
   }
 }
@@ -28,20 +25,28 @@ void generate_matrix(double* matrix, size_t n) {
     }
 }
 
-int main(size_t n) {
-    // Initialize matrixes
-    double *A = malloc(sizeof(double)*n*n);
-    double *B = malloc(sizeof(double)*n*n);
-    double *C = malloc(sizeof(double)*n*n);
-    
-    generate_matrix(A, n);
-    generate_matrix(B, n);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Número incorreto de parâmetros passados. Encerrando programa.\n");
+        return -1;
+    }
+    else {
+        size_t n = atoi(argv[1]);
 
-    dgemm(n, A, B, C);
+        // Initialize matrixes
+        double *A = malloc(sizeof(double)*n*n);
+        double *B = malloc(sizeof(double)*n*n);
+        double *C = malloc(sizeof(double)*n*n);
+        
+        generate_matrix(A, n);
+        generate_matrix(B, n);
 
-    free(A);
-    free(B);
-    free(C);
+        dgemm(n, A, B, C);
+
+        free(A);
+        free(B);
+        free(C);
+    }
 
     return 0;
 }
